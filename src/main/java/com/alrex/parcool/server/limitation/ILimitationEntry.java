e8b7db0ff5a0bc4c35380118bb968822d350e4d8
alRex-U <83890990+alRex-U@users.noreply.github.com>
2026-05-05 00:13:22 +0900

    add automated limitation entries generation

---
+++ src/main/java/com/alrex/parcool/server/limitation/ILimitationEntry.java
@@ -0,0 +1,62 @@
+package com.alrex.parcool.server.limitation;
+
+
+import net.minecraft.util.Mth;
+
+import javax.annotation.Nonnull;
+
+public interface ILimitationEntry<T extends Comparable<T>> {
+    String name();
+
+    @Nonnull
+    T defaultValue();
+
+    PriorityOrder priority();
+
+    String description();
+
+    int index();
+
+    default T clampInValidRange(T value) {
+        return value;
+    }
+
+    ;
+
+    record Bool(
+            int index,
+            String name,
+            Boolean defaultValue,
+            PriorityOrder priority,
+            String description
+    ) implements ILimitationEntry<Boolean> {
+    }
+
+    record Int(
+            int index,
+            String name,
+            Short defaultValue,
+            short min, short max,
+            PriorityOrder priority,
+            String description
+    ) implements ILimitationEntry<Short> {
+        @Override
+        public Short clampInValidRange(Short value) {
+            return (short) Mth.clamp(value, min, max);
+        }
+    }
+
+    record Real(
+            int index,
+            String name,
+            Float defaultValue,
+            float min, float max,
+            PriorityOrder priority,
+            String description
+    ) implements ILimitationEntry<Float> {
+        @Override
+        public Float clampInValidRange(Float value) {
+            return Mth.clamp(value, max, max);
+        }
+    }
+}
