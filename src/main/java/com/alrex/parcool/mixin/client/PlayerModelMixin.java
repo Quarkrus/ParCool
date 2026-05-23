df97bba95723815eca22c2908687a244f67a3112
alRex-U <83890990+alRex-U@users.noreply.github.com>
2026-05-23 18:36:26 +0900

    copy transformations of model into player clothing parts

--- src/main/java/com/alrex/parcool/mixin/client/PlayerModelMixin.java
+++ src/main/java/com/alrex/parcool/mixin/client/PlayerModelMixin.java
@@ -6,13 +6,42 @@ import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(PlayerModel.class)
 public abstract class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
+	@Shadow
+	@Final
+	public ModelPart leftPants;
+
+	@Shadow
+	@Final
+	public ModelPart rightPants;
+
+	@Shadow
+	@Final
+	public ModelPart leftSleeve;
+
+	@Shadow
+	@Final
+	public ModelPart rightSleeve;
+
+	@Shadow
+	@Final
+	public ModelPart jacket;
+
+	@Shadow
+	@Final
+	private List<ModelPart> parts;
+
 	public PlayerModelMixin(ModelPart p_i1148_1_) {
 		super(p_i1148_1_);
 	}
@@ -28,6 +57,7 @@ public abstract class PlayerModelMixin<T extends LivingEntity> extends HumanoidM
 				transform.transformation().transforms().get(AnimatableModelPart.RIGHT_LEG).apply(rightLeg);
 				transform.transformation().transforms().get(AnimatableModelPart.LEFT_ARM).apply(leftArm);
 				transform.transformation().transforms().get(AnimatableModelPart.LEFT_LEG).apply(leftLeg);
+				parCool$copyWearTransformation();
 				info.cancel();
 			}
 		}
@@ -44,6 +74,16 @@ public abstract class PlayerModelMixin<T extends LivingEntity> extends HumanoidM
 			transform.transformation().transforms().get(AnimatableModelPart.RIGHT_LEG).apply(rightLeg, blendingFactor);
 			transform.transformation().transforms().get(AnimatableModelPart.LEFT_ARM).apply(leftArm, blendingFactor);
 			transform.transformation().transforms().get(AnimatableModelPart.LEFT_LEG).apply(leftLeg, blendingFactor);
+			parCool$copyWearTransformation();
 		}
+	}
+
+	@Unique
+	private void parCool$copyWearTransformation() {
+		leftPants.copyFrom(this.leftLeg);
+		rightPants.copyFrom(this.rightLeg);
+		leftSleeve.copyFrom(this.leftArm);
+		rightSleeve.copyFrom(this.rightArm);
+		jacket.copyFrom(this.body);
 	}
 }
