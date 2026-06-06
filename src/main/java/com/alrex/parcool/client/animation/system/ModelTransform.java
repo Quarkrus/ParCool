9f38b088a7a6f2f09db42a531020ca95a9b1849e
alRex-U <83890990+alRex-U@users.noreply.github.com>
2026-06-06 22:20:50 +0900

    swap right and left transform on mirroring

--- src/main/java/com/alrex/parcool/client/animation/system/ModelTransform.java
+++ src/main/java/com/alrex/parcool/client/animation/system/ModelTransform.java
@@ -17,12 +17,22 @@ public record ModelTransform(EnumMap<AnimatableModelPart, Transform> transforms)
 
     public ModelTransform mirror() {
         var newMap = new EnumMap<AnimatableModelPart, Transform>(AnimatableModelPart.class);
-        for (var part : AnimatableModelPart.values()) {
-            var transform = transforms.get(part);
-            if (transform != null) {
-                newMap.put(part, transform.mirror());
-            }
+        var headTransform = transforms.get(AnimatableModelPart.HEAD);
+        if (headTransform != null) {
+            newMap.put(AnimatableModelPart.HEAD, headTransform.mirror());
         }
+        var bodyTransform = transforms.get(AnimatableModelPart.HEAD);
+        if (bodyTransform != null) {
+            newMap.put(AnimatableModelPart.HEAD, bodyTransform.mirror());
+        }
+        var rArmTransform = transforms.get(AnimatableModelPart.RIGHT_ARM);
+        var lArmTransform = transforms.get(AnimatableModelPart.LEFT_ARM);
+        if (lArmTransform != null) newMap.put(AnimatableModelPart.RIGHT_ARM, lArmTransform);
+        if (rArmTransform != null) newMap.put(AnimatableModelPart.LEFT_ARM, rArmTransform);
+        var rLegTransform = transforms.get(AnimatableModelPart.RIGHT_LEG);
+        var lLegTransform = transforms.get(AnimatableModelPart.LEFT_LEG);
+        if (lLegTransform != null) newMap.put(AnimatableModelPart.RIGHT_LEG, lLegTransform);
+        if (rLegTransform != null) newMap.put(AnimatableModelPart.LEFT_LEG, rLegTransform);
         return new ModelTransform(newMap);
     }
 
